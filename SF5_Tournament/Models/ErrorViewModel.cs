817b2fd9f1666e682e4731bcad3297b69e9a999f
Shefat Al Mahmud <shefat2002@gmail.com>
2026-06-24 14:21:34 +0600

    inital commit

---
+++ SF5_Tournament/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SF5_Tournament.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
